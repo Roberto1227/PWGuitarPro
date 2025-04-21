7980ab56cba7083584b1beeedec4f5a2537d03b2
Roberto1227 <roberto2727Chavez@gmail.com>
2025-04-20 20:44:34 -0600

    Avanzados y medios

---
+++ medio.js
@@ -0,0 +1,405 @@
+
+const acordes7Mayores = [
+    {
+      name: "Do7 Mayor",
+      image: "Acordes/Medio/7may-do.jpg",
+      sound: "Acordes/Medio/sonido-7may-do.mp3",
+      description: "El acorde Do7 Mayor combina la triada mayor con una séptima mayor, creando un sonido brillante y elegante.",
+      fingerPosition: "Acordes/Medio/posicion-7may-do.jpg",
+    },
+    {
+      name: "Re7 Mayor",
+      image: "Acordes/Medio/7may-re.jpg",
+      sound: "Acordes/Medio/sonido-7may-re.mp3",
+      description: "El acorde Re7 Mayor aporta un sonido vibrante y lleno de color, ideal para resolver progresiones.",
+      fingerPosition: "Acordes/Medio/posicion-7may-re.jpg",
+    },
+    {
+      name: "Mi7 Mayor",
+      image: "Acordes/Medio/7may-mi.jpg",
+      sound: "Acordes/Medio/sonido-7may-mi.mp3",
+      description: "El acorde Mi7 Mayor aporta un toque elegante y sofisticado a las composiciones musicales.",
+      fingerPosition: "Acordes/Medio/posicion-7may-mi.jpg",
+    },
+    {
+      name: "Fa7 Mayor",
+      image: "Acordes/Medio/7may-fa.jpg",
+      sound: "Acordes/Medio/sonido-7may-fa.mp3",
+      description: "El acorde Fa7 Mayor crea un sonido majestuoso y potente que resalta en cualquier pieza musical.",
+      fingerPosition: "Acordes/Medio/posicion-7may-fa.jpg",
+    },
+    {
+      name: "Sol7 Mayor",
+      image: "Acordes/Medio/7may-sol.jpg",
+      sound: "Acordes/Medio/sonido-7may-sol.mp3",
+      description: "El acorde Sol7 Mayor tiene un timbre cálido y resonante, perfecto para transiciones armónicas.",
+      fingerPosition: "Acordes/Medio/posicion-7may-sol.jpg",
+    },
+    {
+      name: "La7 Mayor",
+      image: "Acordes/Medio/7may-la.jpg",
+      sound: "Acordes/Medio/sonido-7may-la.mp3",
+      description: "El acorde La7 Mayor es versátil y agrega un toque distinguido a las progresiones.",
+      fingerPosition: "Acordes/Medio/posicion-7may-la.jpg",
+    },
+    {
+      name: "Si7 Mayor",
+      image: "Acordes/Medio/7may-si.jpg",
+      sound: "Acordes/Medio/sonido-7may-si.mp3",
+      description: "El acorde Si7 Mayor aporta profundidad y complejidad tonal con su séptima mayor.",
+      fingerPosition: "Acordes/Medio/posicion-7may-si.jpg",
+    },
+  ];
+  
+  const acordes7Menores = [
+    {
+      name: "Do7 Menor",
+      image: "Acordes/Medio/7men-do.jpg",
+      sound: "Acordes/Medio/sonido-7men-do.mp3",
+      description: "El acorde Do7 Menor tiene un tono melancólico y profundo, ideal para añadir un toque sentimental a tu música.",
+      fingerPosition: "Acordes/Medio/posicion-7men-do.jpg",
+    },
+    {
+      name: "Re7 Menor",
+      image: "Acordes/Medio/7men-re.jpg",
+      sound: "Acordes/Medio/sonido-7men-re.mp3",
+      description: "El acorde Re7 Menor es versátil y se utiliza comúnmente en composiciones con un tono introspectivo.",
+      fingerPosition: "Acordes/Medio/posicion-7men-re.jpg",
+    },
+    {
+      name: "Mi7 Menor",
+      image: "Acordes/Medio/7men-mi.jpg",
+      sound: "Acordes/Medio/sonido-7men-mi.mp3",
+      description: "El acorde Mi7 Menor ofrece un sonido rico y cálido, perfecto para baladas y piezas emocionales.",
+      fingerPosition: "Acordes/Medio/posicion-7men-mi.jpg",
+    },
+    {
+      name: "Fa7 Menor",
+      image: "Acordes/Medio/7men-fa.jpg",
+      sound: "Acordes/Medio/sonido-7men-fa.mp3",
+      description: "El acorde Fa7 Menor añade un dramatismo único a tus interpretaciones.",
+      fingerPosition: "Acordes/Medio/posicion-7men-fa.jpg",
+    },
+    {
+      name: "Sol7 Menor",
+      image: "Acordes/Medio/7men-sol.jpg",
+      sound: "Acordes/Medio/sonido-7men-sol.mp3",
+      description: "El acorde Sol7 Menor produce un ambiente relajado y contemplativo.",
+      fingerPosition: "Acordes/Medio/posicion-7men-sol.jpg",
+    },
+    {
+      name: "La7 Menor",
+      image: "Acordes/Medio/7men-la.jpg",
+      sound: "Acordes/Medio/sonido-7men-la.mp3",
+      description: "El acorde La7 Menor es ideal para crear transiciones suaves en progresiones de acordes.",
+      fingerPosition: "Acordes/Medio/posicion-7men-la.jpg",
+    },
+    {
+      name: "Si7 Menor",
+      image: "Acordes/Medio/7men-si.jpg",
+      sound: "Acordes/Medio/sonido-7men-si.mp3",
+      description: "El acorde Si7 Menor aporta una riqueza melódica que destaca en pasajes oscuros o sentimentales.",
+      fingerPosition: "Acordes/Medio/posicion-7men-si.jpg",
+    },
+  ];
+  
+  const acordes6 = [
+    {
+      name: "Do6",
+      image: "Acordes/Medio/sexta-do.jpg",
+      sound: "Acordes/Medio/sonido-sexta-do.mp3",
+      description: "El acorde Do6 añade la sexta al acorde mayor, creando una atmósfera cálida y relajante.",
+      fingerPosition: "Acordes/Medio/posicion-sexta-do.jpg",
+    },
+    {
+      name: "Re6",
+      image: "Acordes/Medio/sexta-re.jpg",
+      sound: "Acordes/Medio/sonido-sexta-re.mp3",
+      description: "El acorde Re6 proporciona un sonido suave y perfecto para acompañamientos melódicos.",
+      fingerPosition: "Acordes/Medio/posicion-sexta-re.jpg",
+    },
+    {
+      name: "Mi6",
+      image: "Acordes/Medio/sexta-mi.jpg",
+      sound: "Acordes/Medio/sonido-sexta-mi.mp3",
+      description: "El acorde Mi6 aporta un toque armónico y cálido a las progresiones musicales.",
+      fingerPosition: "Acordes/Medio/posicion-sexta-mi.jpg",
+    },
+    {
+      name: "Fa6",
+      image: "Acordes/Medio/sexta-fa.jpg",
+      sound: "Acordes/Medio/sonido-sexta-fa.mp3",
+      description: "El acorde Fa6 tiene una tonalidad rica, ideal para piezas con un carácter suave.",
+      fingerPosition: "Acordes/Medio/posicion-sexta-fa.jpg",
+    },
+    {
+      name: "Sol6",
+      image: "Acordes/Medio/sexta-sol.jpg",
+      sound: "Acordes/Medio/sonido-sexta-sol.mp3",
+      description: "El acorde Sol6 genera un ambiente cálido y sofisticado, ideal para acompañamientos.",
+      fingerPosition: "Acordes/Medio/posicion-sexta-sol.jpg",
+    },
+    {
+      name: "La6",
+      image: "Acordes/Medio/sexta-la.jpg",
+      sound: "Acordes/Medio/sonido-sexta-la.mp3",
+      description: "El acorde La6 es versátil y adecuado para crear una atmósfera relajante.",
+      fingerPosition: "Acordes/Medio/posicion-sexta-la.jpg",
+    },
+    {
+      name: "Si6",
+      image: "Acordes/Medio/sexta-si.jpg",
+      sound: "Acordes/Medio/sonido-sexta-si.mp3",
+      description: "El acorde Si6 ofrece un color tonal brillante que enriquece cualquier progresión musical.",
+      fingerPosition: "Acordes/Medio/posicion-sexta-si.jpg",
+    },
+  ];
+  
+  const acordesSuspendidos2 = [
+    {
+      name: "Do Sus2",
+      image: "Acordes/Medio/sus2-do.jpg",
+      sound: "Acordes/Medio/sonido-sus2-do.mp3",
+      description: "El acorde Do Sus2 reemplaza la tercera por la segunda, generando un efecto moderno y abierto.",
+      fingerPosition: "Acordes/Medio/posicion-sus2-do.jpg",
+    },
+    {
+      name: "Re Sus2",
+      image: "Acordes/Medio/sus2-re.jpg",
+      sound: "Acordes/Medio/sonido-sus2-re.mp3",
+      description: "El acorde Re Sus2 agrega una sensación relajada y contemporánea a tus melodías.",
+      fingerPosition: "Acordes/Medio/posicion-sus2-re.jpg",
+    },
+    {
+      name: "Mi Sus2",
+      image: "Acordes/Medio/sus2-mi.jpg",
+      sound: "Acordes/Medio/sonido-sus2-mi.mp3",
+      description: "El acorde Mi Sus2 produce un sonido abierto y versátil ideal para cualquier género musical.",
+      fingerPosition: "Acordes/Medio/posicion-sus2-mi.jpg",
+    },
+    {
+      name: "Fa Sus2",
+      image: "Acordes/Medio/sus2-fa.jpg",
+      sound: "Acordes/Medio/sonido-sus2-fa.mp3",
+      description: "El acorde Fa Sus2 proporciona una atmósfera ligera y armoniosa.",
+      fingerPosition: "Acordes/Medio/posicion-sus2-fa.jpg",
+    },
+    {
+      name: "Sol Sus2",
+      image: "Acordes/Medio/sus2-sol.jpg",
+      sound: "Acordes/Medio/sonido-sus2-sol.mp3",
+      description: "El acorde Sol Sus2 tiene un efecto fresco y moderno, perfecto para acompañamientos.",
+      fingerPosition: "Acordes/Medio/posicion-sus2-sol.jpg",
+    },
+    {
+      name: "La Sus2",
+      image: "Acordes/Medio/sus2-la.jpg",
+      sound: "Acordes/Medio/sonido-sus2-la.mp3",
+      description: "El acorde La Sus2 produce un sonido limpio y abierto que complementa cualquier progresión.",
+      fingerPosition: "Acordes/Medio/posicion-sus2-la.jpg",
+    },
+    {
+      name: "Si Sus2",
+      image: "Acordes/Medio/sus2-si.jpg",
+      sound: "Acordes/Medio/sonido-sus2-si.mp3",
+      description: "El acorde Si Sus2 añade una tonalidad única y brillante a tus composiciones.",
+      fingerPosition: "Acordes/Medio/posicion-sus2-si.jpg",
+    },
+  ];
+
+  const acordesSuspendidos4 = [
+    {
+      name: "Do Sus4",
+      image: "Acordes/Medio/sus4-do.jpg",
+      sound: "Acordes/Medio/sonido-sus4-do.mp3",
+      description: "El acorde Do Sus4 sustituye la tercera por la cuarta, creando tensión y una sensación expectante.",
+      fingerPosition: "Acordes/Medio/posicion-sus4-do.jpg",
+    },
+    {
+      name: "Re Sus4",
+      image: "Acordes/Medio/sus4-re.jpg",
+      sound: "Acordes/Medio/sonido-sus4-re.mp3",
+      description: "El acorde Re Sus4 agrega un elemento de tensión dinámica a cualquier progresión musical.",
+      fingerPosition: "Acordes/Medio/posicion-sus4-re.jpg",
+    },
+    {
+      name: "Mi Sus4",
+      image: "Acordes/Medio/sus4-mi.jpg",
+      sound: "Acordes/Medio/sonido-sus4-mi.mp3",
+      description: "El acorde Mi Sus4 ofrece una atmósfera única que encaja perfectamente en estilos modernos.",
+      fingerPosition: "Acordes/Medio/posicion-sus4-mi.jpg",
+    },
+    {
+      name: "Fa Sus4",
+      image: "Acordes/Medio/sus4-fa.jpg",
+      sound: "Acordes/Medio/sonido-sus4-fa.mp3",
+      description: "El acorde Fa Sus4 crea una sensación de espera y dramatismo en tu música.",
+      fingerPosition: "Acordes/Medio/posicion-sus4-fa.jpg",
+    },
+    {
+      name: "Sol Sus4",
+      image: "Acordes/Medio/sus4-sol.jpg",
+      sound: "Acordes/Medio/sonido-sus4-sol.mp3",
+      description: "El acorde Sol Sus4 tiene un carácter abierto y expresivo que cautiva al oyente.",
+      fingerPosition: "Acordes/Medio/posicion-sus4-sol.jpg",
+    },
+    {
+      name: "La Sus4",
+      image: "Acordes/Medio/sus4-la.jpg",
+      sound: "Acordes/Medio/sonido-sus4-la.mp3",
+      description: "El acorde La Sus4 proporciona un contraste armónico emocionante en tu progresión.",
+      fingerPosition: "Acordes/Medio/posicion-sus4-la.jpg",
+    },
+    {
+      name: "Si Sus4",
+      image: "Acordes/Medio/sus4-si.jpg",
+      sound: "Acordes/Medio/sonido-sus4-si.mp3",
+      description: "El acorde Si Sus4 añade un toque expectante y armonioso a tus composiciones musicales.",
+      fingerPosition: "Acordes/Medio/posicion-sus4-si.jpg",
+    },
+  ];
+  
+  const acordesDisminuidos = [
+    {
+      name: "Do Dim",
+      image: "Acordes/Medio/dis-do.jpg",
+      sound: "Acordes/Medio/sonido-dis-do.mp3",
+      description: "El acorde Do Dim combina una triada disminuida con una séptima menor, aportando un carácter oscuro y misterioso.",
+      fingerPosition: "Acordes/Medio/posicion-dis-do.jpg",
+    },
+    {
+      name: "Re Dim",
+      image: "Acordes/Medio/dis-re.jpg",
+      sound: "Acordes/Medio/sonido-dis-re.mp3",
+      description: "El acorde Re Dim añade dramatismo y tensión a tus progresiones musicales.",
+      fingerPosition: "Acordes/Medio/posicion-dis-re.jpg",
+    },
+    {
+      name: "Mi Dim",
+      image: "Acordes/Medio/dis-mi.jpg",
+      sound: "Acordes/Medio/sonido-dis-mi.mp3",
+      description: "El acorde Mi Dim transmite incertidumbre y puede ser utilizado para crear pasajes melancólicos.",
+      fingerPosition: "Acordes/Medio/posicion-dis-mi.jpg",
+    },
+    {
+      name: "Fa Dim",
+      image: "Acordes/Medio/dis-fa.jpg",
+      sound: "Acordes/Medio/sonido-dis-fa.mp3",
+      description: "El acorde Fa Dim destaca en estilos de música que buscan un tono inquietante o introspectivo.",
+      fingerPosition: "Acordes/Medio/posicion-dis-fa.jpg",
+    },
+    {
+      name: "Sol Dim",
+      image: "Acordes/Medio/dis-sol.jpg",
+      sound: "Acordes/Medio/sonido-dis-sol.mp3",
+      description: "El acorde Sol Dim crea un ambiente misterioso y profundo, ideal para piezas dinámicas.",
+      fingerPosition: "Acordes/Medio/posicion-dis-sol.jpg",
+    },
+    {
+      name: "La Dim",
+      image: "Acordes/Medio/dis-la.jpg",
+      sound: "Acordes/Medio/sonido-dis-la.mp3",
+      description: "El acorde La Dim añade un toque de tensión y dramatismo que resalta en progresiones menores.",
+      fingerPosition: "Acordes/Medio/posicion-dis-la.jpg",
+    },
+    {
+      name: "Si Dim",
+      image: "Acordes/Medio/dis-si.jpg",
+      sound: "Acordes/Medio/sonido-dis-si.mp3",
+      description: "El acorde Si Dim es perfecto para transmitir emociones intensas y añadir contraste en tus piezas.",
+      fingerPosition: "Acordes/Medio/posicion-dis-si.jpg",
+    },
+  ];
+
+  const acordesAumentados = [
+    {
+      name: "Do Aug",
+      image: "Acordes/Medio/aum-do.jpg",
+      sound: "Acordes/Medio/sonido-aum-do.mp3",
+      description: "El acorde Do Aug incorpora una quinta aumentada, creando un sonido expansivo y misterioso.",
+      fingerPosition: "Acordes/Medio/posicion-aum-do.jpg",
+    },
+    {
+      name: "Re Aug",
+      image: "Acordes/Medio/aum-re.jpg",
+      sound: "Acordes/Medio/sonido-aum-re.mp3",
+      description: "El acorde Re Aug aporta un carácter enigmático y versátil a tus composiciones.",
+      fingerPosition: "Acordes/Medio/posicion-aum-re.jpg",
+    },
+    {
+      name: "Mi Aug",
+      image: "Acordes/Medio/aum-mi.jpg",
+      sound: "Acordes/Medio/sonido-aum-mi.mp3",
+      description: "El acorde Mi Aug brinda una tonalidad expansiva que evoca emociones complejas.",
+      fingerPosition: "Acordes/Medio/posicion-aum-mi.jpg",
+    },
+    {
+      name: "Fa Aug",
+      image: "Acordes/Medio/aum-fa.jpg",
+      sound: "Acordes/Medio/sonido-aum-fa.mp3",
+      description: "El acorde Fa Aug crea un sonido envolvente y único, perfecto para momentos destacados.",
+      fingerPosition: "Acordes/Medio/posicion-aum-fa.jpg",
+    },
+    {
+      name: "Sol Aug",
+      image: "Acordes/Medio/aum-sol.jpg",
+      sound: "Acordes/Medio/sonido-aum-sol.mp3",
+      description: "El acorde Sol Aug añade un toque expansivo y dinámico que destaca en progresiones.",
+      fingerPosition: "Acordes/Medio/posicion-aum-sol.jpg",
+    },
+    {
+      name: "La Aug",
+      image: "Acordes/Medio/aum-la.jpg",
+      sound: "Acordes/Medio/sonido-aum-la.mp3",
+      description: "El acorde La Aug proporciona una armonía enriquecedora y poderosa.",
+      fingerPosition: "Acordes/Medio/posicion-aum-la.jpg",
+    },
+    {
+      name: "Si Aug",
+      image: "Acordes/Medio/aum-si.jpg",
+      sound: "Acordes/Medio/sonido-aum-si.mp3",
+      description: "El acorde Si Aug ofrece un sonido cautivador y profundo, ideal para estilos experimentales.",
+      fingerPosition: "Acordes/Medio/posicion-aum-si.jpg",
+    },
+  ];
+
+  function showModal(event) {
+    if (currentAudio) {
+      currentAudio.pause();
+      currentAudio.currentTime = 0;
+    }
+  
+    const lessonType = event.target.getAttribute("data-modal");
+    console.log("Botón presionado:", lessonType);
+  
+    if (lessonType === "acordes7Mayores") {
+      chords = acordes7Mayores;
+    } else if (lessonType === "acordes7Menores") {
+      chords = acordes7Menores;
+    } else if (lessonType === "acordes6") {
+      chords = acordes6;
+    } else if (lessonType === "acordesSuspendidos2") {
+      chords = acordesSuspendidos2;
+    } else if (lessonType === "acordesSuspendidos4") {
+      chords = acordesSuspendidos4;
+    } else if (lessonType === "acordesDisminuidos") {
+      chords = acordesDisminuidos;
+    } else if (lessonType === "acordesAumentados") {
+      chords = acordesAumentados;
+    } else {
+      console.error("Lección no encontrada: ", lessonType);
+      return;
+    }
+  
+    currentChordIndex = 0;
+    lessonModal.classList.remove("hidden");
+    introSlide.style.display = "block";
+    chordSlide.style.display = "none";
+    finalSlide.style.display = "none";
+  
+    const lessonNameText = lessonType.includes("Suspendidos")
+      ? `Lección: ${lessonType.replace("acordes", "").replace(/\d/g, "").trim()}`
+      : `Lección: ${lessonType}`;
+  
+    document.getElementById("lessonName").textContent = lessonNameText;
+  }
